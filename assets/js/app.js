a1e2e405a19780a4353fc1f1a8d1169215ec3626
Lord Tapulao <drolxiv@gmail.com>
2021-03-04 01:50:29 -0700

    ourstory

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,17 @@
-var sideMenuValue= "0";
+var sideMenuValue= 0;
 function sideMenu(){
     if(sideMenuValue==0){
         document.querySelector('.iconMenu').style = "opacity:0; transition:0.5s;";
         document.querySelector('aside').style = "left:0px; transition:0.5s;";
         document.querySelector('.discussionBox').style = "right:-295px; transition:0.5s;";
-        document.querySelector('.tab').style = "transform:rotate(0deg); transition:0.5s;";
+        document.querySelector('.tab i').style = "transform:rotate(0deg); transition:0.5s;";
         setTimeout(function(){ 
             document.querySelector('.mainMenu').style = "opacity:1; transition:0.5s;";
             document.querySelector('aside img').style = "opacity:1; transition:0.5s;";
         }, 500);
         document.querySelector('.hamb').style = "transform:rotate(360deg); opacity:0; transition:0.5s;";
         document.querySelector('.close').style = "transform:rotate(360deg); opacity:1;transition:0.5s;";
-        document.querySelector('.discussionBox').style = "right:-295px; transition:0.5s;";
-        document.querySelector('.tab').style = "transform:rotate(0deg); transition:0.5s;";
-        discussionValue=1;
+        discussionValue=0;
 
         sideMenuValue++;
     }else{
@@ -42,9 +40,9 @@ function turnOffComments(){
     }
 }
 
-var settingsValue = "0";
+var settingsValue = 0;
 function showSettings(){
-    if(settingsValue== "0"){
+    if(settingsValue== 0){
         resetUserSettings();
         document.querySelector('.settingsDropDown').style = "height:160px;width:300px; opacity:1; transition:0.5s;";
         settingsValue++;
@@ -54,9 +52,9 @@ function showSettings(){
     }
 }
 
-var usersValue = "0";
+var usersValue = 0;
 function showUserSettings(){
-    if(usersValue== "0"){
+    if(usersValue== 0){
         resetSettings();
         document.querySelector('.userDropDown').style = "height:100px;width:400px; opacity:1; transition:0.5s;";
         usersValue++;
@@ -68,30 +66,34 @@ function showUserSettings(){
 
 function resetSettings(){
     document.querySelector('.settingsDropDown').style = "height:0px;width:0px; opacity:0; transition:0.5s;";
-    settingsValue= "0";
+    settingsValue= 0;
 }
 
 function resetUserSettings(){
     document.querySelector('.userDropDown').style = "height:0px;width:0px; opacity:0; transition:0.5s;";
-    usersValue = "0";
+    usersValue = 0;
 }
 
-var discussionValue = "0";
+var discussionValue = 0;
 function discussionBox(){
-    if(discussionValue== "0"){
+    if(discussionValue== 0){
         document.querySelector('.discussionBox').style = "right:0px; transition:0.5s;";
         document.querySelector('.tab i').style = "transform:rotate(180deg); transition:0.5s;";
         discussionValue++;
-    }else{
-        document.querySelector('.discussionBox').style = "right:-295px; transition:0.5s;";
-        document.querySelector('.tab i').style = "transform:rotate(0deg); transition:0.5s;";
-        document.querySelector('aside').style = "left:-250px;transition:0.5s;";
+
+
+        document.querySelector('aside').style = "left:-150px;transition:0.5s;";
         document.querySelector('aside img').style = "opacity:0; transition:0.5s;";
+        document.querySelector('.mainMenu').style = "opacity:0; transition:0.5s;";
         setTimeout(function(){ 
             document.querySelector('.iconMenu').style = "opacity:1; transition:0.5s;";
         }, 500);
-        document.querySelector('.mainMenu').style = "opacity:0; transition:0.5s;";
-        document.querySelector('.close').style = "transform:rotate(360deg); opacity:0;transition:0.5s;";
-        discussionValue="0";
+        document.querySelector('.hamb').style = "transform:rotate(0deg); opacity:1; transition:0.5s;";
+        document.querySelector('.close').style = "transform:rotate(0deg); opacity:0; transition:0.5s;";
+        sideMenuValue=0;
+    }else{
+        document.querySelector('.discussionBox').style = "right:-295px; transition:0.5s;";
+        document.querySelector('.tab i').style = "transform:rotate(0deg); transition:0.5s;";
+        discussionValue=0;
     }
 }
